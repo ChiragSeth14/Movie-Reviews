07d7c2acf5f09a2d9b9afeda52a979bd1fc90a8f
ChiragSeth14 <135326086+ChiragSeth14@users.noreply.github.com>
2024-10-17 04:04:35 -0700

    backend code complete

---
+++ src/main/java/com/movie_reviews/movies/dao/MovieRepository.java
@@ -0,0 +1,14 @@
+package com.movie_reviews.movies.dao;
+
+import com.movie_reviews.movies.object.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
